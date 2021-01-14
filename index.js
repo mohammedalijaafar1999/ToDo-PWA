80e440e1c03d2b15e673ba99c1c4da2cc418be71
Mohammed Ali Jaafar <mohammedalijaafar1999@gmail.com>
2021-01-14 09:08:53 +0300

    project will be on hold until learned pwa with node js

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.send('An alligator approaches!');
+    //res.send('An alligator approaches!');
+    res.render('index')
 });
 
 app.listen(3000, () => console.log('app listening on port 3000!'));
